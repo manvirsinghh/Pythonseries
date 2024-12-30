e5cb3b4308b7e3e6e867cadb729d78d13bbe309c
manvirsinghh <manvirsingh6656@gmail.com>
2024-12-30 21:44:36 +0530

    how to get multiple inputs from user

---
+++ takemultipleinputsfromuser.py
@@ -0,0 +1,16 @@
+x,y,z=input("Values:").split()
+print(x)
+print(y)
+print(z)
+
+
+a,b,c=input("values:").split()
+print(a)
+print(b)
+print(c)
+
+
+name1,name2,name3=input("names:").split()
+print(name1)
+print(name2)
+print(name3)
