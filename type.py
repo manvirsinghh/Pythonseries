18b831a02129acc362224d15a1d2896fe9316143
manvirsinghh <manvirsingh6656@gmail.com>
2024-12-30 21:42:50 +0530

    how to find type of variable

---
+++ type.py
@@ -0,0 +1,10 @@
+name="manvir singh"
+print(type(name))
+num1=24
+print(type(num1))
+# num2=int(input("enter the value:"))
+# print(num2)
+# print(type(num2))
+num3=float(input("enter value of num3:"))
+print(num3)
+print(type(num3))
