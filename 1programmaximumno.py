10e3bc860df22738b017ff560b4bc7ea5ca93a20
manvirsinghh <manvirsingh6656@gmail.com>
2025-01-01 21:45:09 +0530

    msg

---
+++ 1programmaximumno.py
@@ -0,0 +1,15 @@
+a=2
+b=3
+if a>b:
+     print(a)
+else:
+    print(b)
+
+    #2d way
+    print(max(a,b))
+
+    #3rd method using ternary operator
+    c=3
+    d=6
+    res=c if c>d else d
+    print(res)
