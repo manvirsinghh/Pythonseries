c36889779af2cf93c46a6e03e13828c0c495d59f
manvirsinghh <manvirsingh6656@gmail.com>
2025-01-05 13:32:39 +0530

    programs

---
+++ listcomprehemsion.py
@@ -0,0 +1,19 @@
+# #list comprehension is used to make list using concise syntax. we have to apply expression on each item in list to create new list in given range
+# list=[2,4,5,7,8]
+# res=[val **2 for val in list]
+# print(res)
+
+# a=[1,2,3,4,5,6]
+# res=[val for val in a if val%2==0]
+# print(res)
+
+
+#ternary operator
+#suppose want to check wheether no is even or odd we can use ternay operator over here to check this
+num=5
+res="even"if num%2==0 else "odd"
+print(res)
+#max of two numbers
+num1=6
+num2=10
+
